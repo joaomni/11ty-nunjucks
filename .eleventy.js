93baede4c80e06a9ce190f4315166d5135cfa529
João Moreira <joaojpmoreira25@gmail.com>
2023-11-01 05:23:35 +0000

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,5 @@ module.exports = function (eleventyConfig) {
         input: "src",
         output: "public",
       },
-      plugins: [
-        "11ty-minify-html",
-        "11ty-minify-css",
-        "11ty-minify-js",
-        "11ty-bundler"
-      ]
     };
   };
